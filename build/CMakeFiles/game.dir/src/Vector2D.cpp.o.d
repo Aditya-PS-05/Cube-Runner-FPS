272d40449a30863dbe663236e4c7e8c07864689d
Aditya-PS-05 <adityapratapsjnhh7654@gmail.com>
2025-02-20 19:02:54 +0530

    chore: Remove build directory from git tracking

--- build/CMakeFiles/game.dir/src/Vector2D.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/game.dir/src/Vector2D.cpp.o: \
- /home/aditya/my-work/clishootinggame/src/Vector2D.cpp \
- /usr/include/stdc-predef.h \
- /home/aditya/my-work/clishootinggame/include/Vector2D.h
